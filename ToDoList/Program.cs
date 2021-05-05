e182ac047400af061c845cf9731425747b230a9b
Patrick Lee <patricklee1138@live.com>
2021-05-05 11:30:37 -0700

    :construction: UI color tweaks
    Co-authored-by: Nick Reeder <nickreeder32@gmail.com>

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -8,31 +8,45 @@ public class Program
     Console.WriteLine("Welcome to the To-Do List.");
     string lastAdded = "";
     Start:
-    Console.WriteLine(lastAdded + "Would you like to add an item to your list or view your list? (Add/View)");
+    Console.BackgroundColor = ConsoleColor.Black;  
+    Console.ForegroundColor = ConsoleColor.White; 
+    List<Item> items = Item.GetAll();
+    Console.Write("\n" + lastAdded + "Would you like to add an item to your list or view your list? (Add/View) ");
     string userInput = Console.ReadLine();
     if (userInput.ToLower() == "add" || userInput.ToLower() == "a") 
     {
       Console.WriteLine("\n" + "Please enter the description for the new item.");
       string description = Console.ReadLine();
       Item newItem = new Item(description);
-      List<Item> items = Item.GetAll();
-      lastAdded = ("\n" + items[items.Count - 1].Description) + " has been added to your list. ";
+      lastAdded = ("\n" + items[items.Count - 1].Description) + " has been added to your list. " + "\n";
       // Console.WriteLine("{0} has been added to your list.", items[items.Count - 1].Description);
       goto Start;
     }
     else if (userInput.ToLower() == "view" || userInput.ToLower() == "v")
     {
+      if (items.Count == 0)
+      {
+        Console.ForegroundColor = ConsoleColor.Red; 
+        Console.WriteLine("\n" + "Your To-Do List is currently empty. Please add some items.");
+        goto Start;
+      }
       Console.WriteLine("\n" + "Your To-Do List:" + "\n----------------");
       int count = 1;
-      foreach (Item thisItem in Item.GetAll())
+     
+      foreach (Item thisItem in items)
       {
-        Console.WriteLine("{0}", count + ". " + thisItem.Description);
+        Console.BackgroundColor = ConsoleColor.White;  
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.Write("{0}", count + ". " + thisItem.Description + "\n");
         count++;
       }
+      lastAdded = "";
+      goto Start;
     }
     else
     {
       Console.WriteLine("\n" + "Not a recognized input.");
+      goto Start;
     }
   }
 }
