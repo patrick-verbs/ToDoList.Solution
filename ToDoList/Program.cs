f39d21131c9c16a55b078f6bf9523d41602071d4
Patrick Lee <patricklee1138@live.com>
2021-05-05 11:10:47 -0700

    change UI formatting
    Co-authored-by: Nick Reeder <nickreeder32@gmail.com>

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -5,26 +5,34 @@ public class Program
 {
   static void Main()
   {
-    Console.WriteLine("Welcome to the To Do List.");
+    Console.WriteLine("Welcome to the To-Do List.");
+    string lastAdded = "";
     Start:
-    Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View)");
+    Console.WriteLine(lastAdded + "Would you like to add an item to your list or view your list? (Add/View)");
     string userInput = Console.ReadLine();
-    if (userInput.ToLower() == "add") 
+    if (userInput.ToLower() == "add" || userInput.ToLower() == "a") 
     {
-      Console.WriteLine("Please enter the description for the new item.");
+      Console.WriteLine("\n" + "Please enter the description for the new item.");
       string description = Console.ReadLine();
       Item newItem = new Item(description);
       List<Item> items = Item.GetAll();
-      Console.WriteLine("{0} has been added to your list.", items[items.Count - 1].Description);
+      lastAdded = ("\n" + items[items.Count - 1].Description) + " has been added to your list. ";
+      // Console.WriteLine("{0} has been added to your list.", items[items.Count - 1].Description);
       goto Start;
     }
-    else 
+    else if (userInput.ToLower() == "view" || userInput.ToLower() == "v")
     {
-      Console.WriteLine("Here is your to-do list");
+      Console.WriteLine("\n" + "Your To-Do List:" + "\n----------------");
+      int count = 1;
       foreach (Item thisItem in Item.GetAll())
       {
-        Console.WriteLine("{0}", thisItem.Description);
+        Console.WriteLine("{0}", count + ". " + thisItem.Description);
+        count++;
       }
+    }
+    else
+    {
+      Console.WriteLine("\n" + "Not a recognized input.");
     }
   }
 }
